b73dd721bdafad8519447a13a442100aca0189d9
Yap Yee Qiang <yapyeeqiang@gmail.com>
2021-08-14 23:02:08 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+	<Navbar />
+	<section class="flex flex-col items-center mt-20">
+		<!-- Info  -->
+		<div class="mb-6 text-center">
+			<div class="space-y-1">
+				<span class="text-[#f56300]">New</span>
+				<h2 class="text-[32px] font-medium">iMac</h2>
+				<p class="text-[21px] font-medium">Say hello.</p>
+				<p class="text-[17px] font-medium">From $1299</p>
+			</div>
+			<div class="flex flex-col items-center space-y-3 mt-4">
+				<a
+					class="
+						bg-[#0071e3]
+						w-16
+						text-center text-white
+						px-4
+						py-2
+						rounded-full
+					"
+					href="#"
+					>Buy</a
+				>
+				<a class="text-[#06c]" href="#"
+					><span class="hover:underline">Learn more</span> &gt;</a
+				>
+			</div>
+		</div>
+
+		<!-- Image  -->
+		<div class="md:hidden">
+			<img
+				src="https://www.apple.com/v/mac/home/bi/images/overview/hero_imac_24__eq5phrdpwjyq_small.jpg"
+				alt=""
+			/>
+		</div>
+		<div class="hidden md:block">
+			<img
+				src="https://www.apple.com/v/mac/home/bi/images/overview/hero_imac_24__eq5phrdpwjyq_large.jpg"
+				alt=""
+			/>
+		</div>
+	</section>
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue';
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,80 @@
+<template>
+	<nav class="bg-[#3a3a3a]">
+		<div
+			class="
+				max-w-7xl
+				mx-auto
+				flex
+				items-center
+				justify-between
+				px-3
+				space-x-10
+			"
+		>
+			<div class="md:hidden">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="icon"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 8h16M4 16h16"
+					/>
+				</svg>
+			</div>
+			<div>
+				<img
+					src="https://www.apple.com/ac/globalnav/6/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_apple_image__cxwwnrj0urau_large.svg"
+					alt="Apple Logo"
+				/>
+			</div>
+			<div class="hidden md:flex flex-grow justify-between">
+				<a class="link" href="#">Store</a>
+				<a class="link" href="#">Mac</a>
+				<a class="link" href="#">iPad</a>
+				<a class="link" href="#">iPhone</a>
+				<a class="link" href="#">Watch</a>
+				<a class="link" href="#">TV</a>
+				<a class="link" href="#">Music</a>
+				<a class="link" href="#">Support</a>
+			</div>
+			<div class="hidden md:block">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="icon"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+					/>
+				</svg>
+			</div>
+			<div>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="icon"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+					/>
+				</svg>
+			</div>
+		</div>
+	</nav>
+</template>
